4866983864801a08c482a79329367f76945b771b
David Q. Jin <quan.jin@me.com>
2019-12-16 19:43:15 -0600

    added customized typography theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thinking Out Loud`,
+    description: `Personal Blog`,
+    author: `David Q. JIn <david.q.jin@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +16,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,8 +28,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from 'typography';
+import theme from 'typography-theme-wordpress-2016';
+
+const typography = new Typography({
+  ...theme,
+  headerFontFamily: ['Montserrat', 'sans-serif'],
+});
+
+export default typography;
