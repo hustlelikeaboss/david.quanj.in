b7e95472d62f580b4b2d25c4f72da39922d80124
David Q. Jin <quan.jin@me.com>
2019-12-19 17:19:48 -0600

    added lightbox and gallery nav

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Gallery = () => {
-  const photos = Array(30).fill(1);
+  const [show, setShow] = useState(false);
+  const [selectedImg, setSelectedImg] = useState({});
+  const openLightbox = (id, src) => {
+    setShow(true);
+    setSelectedImg({
+      id,
+      src,
+    });
+  };
+  const closeLightbox = () => {
+    setShow(false);
+    setSelectedImg({});
+  };
+
+  const photos = Array(30)
+    .fill(1)
+    .map((val, idx) => {
+      const src =
+        idx % 2 === 0
+          ? `https://picsum.photos/seed/picsum-${idx}/600/800`
+          : `https://picsum.photos/seed/picsum-${idx}/800/600`;
+      return {
+        id: idx + 1,
+        src,
+      };
+    });
+
+  const navTo = id => {
+    const img = photos.find(p => p.id === id);
+    setSelectedImg(img);
+  };
+
   return (
-    <section id="gallery" className="gallery-grid">
-      {photos.map((p, idx) => {
-        return (
-          <figure key={idx} className={`gallery-item gallery-item--${idx + 1}`}>
+    <section id="gallery">
+      <div className="lightbox" style={{ display: show ? 'block' : 'none' }}>
+        <span className="lightbox-close" onClick={() => closeLightbox()}>
+          &times;
+        </span>
+        <div className="lightbox-content">
+          <span className="left" onClick={() => navTo(selectedImg.id - 1)}>
+            &larr;
+          </span>
+          <img src={selectedImg.src} alt="" />
+          <span className="right" onClick={() => navTo(selectedImg.id + 1)}>
+            &rarr;
+          </span>
+        </div>
+      </div>
+      <div className="gallery-grid">
+        {photos.map(photo => (
+          <figure
+            key={photo.id}
+            className={`gallery-item gallery-item--${photo.id + 1}`}
+          >
             <img
-              src={
-                idx % 2 === 0
-                  ? `https://picsum.photos/seed/picsum-${idx}/600/800`
-                  : `https://picsum.photos/seed/picsum-${idx}/800/600`
-              }
+              src={photo.src}
               alt=""
+              onClick={() => openLightbox(photo.id, photo.src)}
             />
           </figure>
-        );
-      })}
+        ))}
+      </div>
     </section>
   );
 };
