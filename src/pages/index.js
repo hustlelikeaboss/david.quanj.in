7c8897e93676e73c5e26bd565bc0f509728f44cf
David Q. Jin <quan.jin@me.com>
2020-02-17 21:31:22 -0600

    link to my other websites for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer';
 import artist from '../images/artist.svg';
 import geek from '../images/geek.svg';
 import hiker from '../images/hiker.svg';
-import profile from '../images/profile.jpg'
+import profile from '../images/profile.jpg';
 import '../styles/main.scss';
 
 const IndexPage = () => (
@@ -16,15 +16,29 @@ const IndexPage = () => (
     <SEO title="Home" />
     <main>
       <section className="flex-container--persona">
-        <Link to="/#portfolio" className="no-underline">
+        {/* <Link to="/#portfolio" className="no-underline">
           <img src={artist} alt="designer" className="persona--1" />
-        </Link>
+        </Link> */}
+        <a
+          href="https://www.hustlelikeaboss.design/"
+          className="no-underline"
+          target="_blank"
+        >
+          <img src={artist} alt="designer" className="persona--1" />
+        </a>
         <Link to="/#profile" className="no-underline">
           <img src={geek} alt="developer" className="persona--2" />
         </Link>
-        <Link to="/#gallery" className="no-underline">
+        {/* <Link to="/#gallery" className="no-underline">
           <img src={hiker} alt="photographer" className="persona--3" />
-        </Link>
+        </Link> */}
+        <a
+          href="/https://www.choicejadephotography.com/"
+          className="no-underline"
+          target="_blank"
+        >
+          <img src={hiker} alt="photographer" className="persona--3" />
+        </a>
       </section>
 
       <section id="profile">
@@ -49,9 +63,9 @@ const IndexPage = () => (
               software developer who writes clean, well tested code; is always
               learning and experimenting; enjoys working with a variety of
               languages, frameworks & cloud technologies in startup or
-              enterprise environments; and has a passion for developing applications
-              that solve business needs, delight end users & make a lasting
-              impact.
+              enterprise environments; and has a passion for developing
+              applications that solve business needs, delight end users & make a
+              lasting impact.
             </p>
             <p className="card-footer">
               <a
